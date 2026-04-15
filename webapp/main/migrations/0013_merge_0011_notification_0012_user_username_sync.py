7ade13b208cff1dbd081962ec95d073ba8e251e0
Gacu <164901031+Gacu33@users.noreply.github.com>
2026-04-15 22:16:47 +0200

    poprawka

---
+++ webapp/main/migrations/0013_merge_0011_notification_0012_user_username_sync.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("main", "0011_notification"),
+        ("main", "0012_user_username_sync"),
+    ]
+
+    operations = []
